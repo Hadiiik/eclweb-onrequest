7f7459de5bec3dae8fb7491d0020fcab06561653
hadiikaddour <hadyqdwr77@gmail.com>
2025-11-25 15:22:59 +0300

    edit manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,25 +2,36 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'ECL-WEB',
-    short_name: 'ecl',
-    description: "educational creative link",
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#f0fdf4',
-    theme_color: '#166534',
-    icons: [
-      {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-    "lang": "ar",
-  }
+  name: "ECL-WEB",
+  short_name: "ecl",
+  description: "educational creative link",
+  start_url: "/",
+  display: "standalone",
+  background_color: "#ffffff",
+  theme_color: "#128d3f",
+  icons: [
+    {
+      src: "/icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png"
+    },
+    {
+      src: "/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png"
+    }
+  ],
+  lang: "ar",
+  categories: [
+    "books",
+    "education"
+  ],
+  shortcuts: [
+    {
+      name: "الأخبار",
+      url: "/news",
+      description: "لأن التعليم يستحق المتابعة؛ نرصد الحدث.. نصنع الوعي!"
+    }
+  ]
+}
 }
